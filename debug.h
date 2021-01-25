fd89e6087cb6daed1544ea1601755a02efdb3fa7
Carlo Wood <carlo@alinoe.com>
2021-01-25 07:36:00 +0100

    In debug mode, automatically use T::print_on when writing to an ostream, if it exists.

--- debug.h
+++ debug.h
@@ -5,8 +5,28 @@
 #define NAMESPACE_DEBUG_END }
 #include "cwds/debug.h"
 
+#ifdef CWDEBUG
 #include "utils/print_using.h"
 namespace libcwd {
 // Allow using PrintUsing inside Dout without the utils:: prefix.
 using utils::print_using;
 } // namespace libcwd
+
+namespace {
+
+template<typename T, typename = void>
+constexpr bool has_print_on = false;
+
+template<typename T>
+constexpr bool has_print_on<T, std::void_t<decltype(std::declval<T>().print_on(std::declval<std::ostream&>()))>> = true;
+
+} // namespace
+
+template<typename T>
+std::enable_if_t<has_print_on<T const>, std::ostream&>
+operator<<(std::ostream& os, T const& data)
+{
+  data.print_on(os);
+  return os;
+}
+#endif
