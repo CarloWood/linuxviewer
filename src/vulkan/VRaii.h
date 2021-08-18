beb51fc635709a46d2c104d288c964c5e75ba313
Carlo Wood <carlo@alinoe.com>
2021-08-18 14:23:27 +0200

    Reformat code, fix comments.

--- src/vulkan/VRaii.h
+++ src/vulkan/VRaii.h
@@ -7,108 +7,75 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
+
 #include <functional>
 
 namespace vulkan {
 
-/**
-* This RAII class is used to hold Vulkan handles; just like a std::unique_ptr, but for Vulkan handles instead of pointers
-* It will call deletef upon destruction
-* It also support move operations, which is implemented by swapping, which will correctly destruct the old resource,
-* since the right value argument get destructed right after the operation
-* It does not support copy operation
-*/
+// This RAII class is used to hold Vulkan handles; just like a std::unique_ptr, but for Vulkan handles instead of pointers.
+//
+// It will call deleter upon destruction.
+// It also support move operations, implemented by swapping, which will correctly destruct the old resource,
+// since the right value argument get destructed right after the operation
+// It does not support copy operation
+//
+// Typical usage:
+//
+//   vulkan::VRaii<vk::DescriptorSetLayout> camera_descriptor_set_layout
+//
+//   You can allocate VRaii as class member in advance and then when you are allocating the resource
+//
+//   camera_descriptor_set_layout = vulkan::VRaii<vk::DescriptorSetLayout>(
+//	device.createDescriptorSetLayout(create_info, nullptr),
+//	[device = this->device](auto& layout){ device.destroyDescriptorSetLayout(layout); }
+//   )
+//
 template <typename T>
 class VRaii
 {
-public:
-	using obj_t = T;
+ private:
+  T object;
+  std::function<void(T&)> deleter;
 
-	VRaii()
-		: object(nullptr)
-		, deleter([](T&) {})
-	{}
+ public:
+  using obj_t = T;
 
-	VRaii(T obj, std::function<void(T&)> deleter)
-		: object(obj)
-		, deleter(deleter)
-	{}
+  VRaii() : object(nullptr), deleter([](T&) {}) {}
+  VRaii(T obj, std::function<void(T&)> deleter) : object(obj), deleter(deleter) {}
+  ~VRaii() { cleanup(); }
 
-	~VRaii()
-	{
-		cleanup();
-	}
+  VRaii<T>& operator=(VRaii<T> const&) = delete;
+  VRaii(VRaii<T> const&)               = delete;
 
-	T& get()
-	{
-		return object;
-	}
+  // Move constructor.
+  VRaii(VRaii<T>&& other) :
+    object(nullptr),    // To be swapped to "other".
+    deleter([](T&) {})  // deleter will be copied in case there is still use for the old container.
+  {
+    swap(*this, other);
+  }
 
-	const T& get() const
-	{
-		return object;
-	}
+  T& get() { return object; }
+  const T& get() const { return object; }
 
-        operator T const&() const
-        {
-          return object;
-        }
+  operator T const &() const { return object; }
 
-	T* data()
-	{
-		return &object;
-	}
+  T* data() { return &object; }
 
-	//operator T() const
-	//{
-	//	return object;
-	//}
+  VRaii<T>& operator=(VRaii<T>&& other)
+  {
+    swap(*this, other);
+    return *this;
+  }
 
-	VRaii(VRaii<T>&& other)
-		: object(nullptr) // to be swapped to "other"
-		, deleter([](T&) {}) // deleter will be copied in case there is still use for the old container
-	{
-		swap(*this, other);
-	}
-	VRaii<T>& operator=(VRaii<T>&& other)
-	{
-		swap(*this, other);
-		return *this;
-	}
-	friend void swap(VRaii<T>& first, VRaii<T>& second)
-	{
-		using std::swap;
-		swap(first.object, second.object);
-		swap(first.deleter, second.deleter);
-	}
+  friend void swap(VRaii<T>& first, VRaii<T>& second)
+  {
+    std::swap(first.object, second.object);
+    std::swap(first.deleter, second.deleter);
+  }
 
-	VRaii<T>& operator=(const VRaii<T>&) = delete;
-	VRaii(const VRaii<T>&) = delete;
-
-private:
-	T object;
-	std::function<void(T&)> deleter;
-
-	void cleanup()
-	{
-		deleter(object);
-	}
+ private:
+  void cleanup() { deleter(object); }
 };
 
-// Typical usage:
-//
-//	vulkan::VRaii<vk::DescriptorSetLayout> camera_descriptor_set_layout
-//
-//	You can allocate VRaii as class member in advance and then when you are allocating the resource
-//
-//	camera_descriptor_set_layout = vulkan::VRaii<vk::DescriptorSetLayout>(
-//		device.createDescriptorSetLayout(create_info, nullptr),
-//		[device = this->device](auto& layout){ device.destroyDescriptorSetLayout(layout); }
-//	)
-//
-//	this is for vulkan.hpp bindings. You can also use this for vulkan.h bindings but you need a temp 
-//	  handle to pass in address for resource allocation functions to accept the handle value first.
-//
-// Of course, you should guarantee the vk::device is released after this handle.
-
-} // namespace vulkan
+}  // namespace vulkan
