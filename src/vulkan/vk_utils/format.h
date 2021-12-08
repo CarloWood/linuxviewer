bbc0c0589a37c030db13b9519eabe1ddc8d00aca
Carlo Wood <carlo@alinoe.com>
2021-12-08 02:25:01 +0100

    Be gone FIXME's

--- src/vulkan/vk_utils/format.h
+++ src/vulkan/vk_utils/format.h
@@ -1,17 +1,21 @@
 #pragma once
 
+#include "debug.h"
+#if CW_DEBUG
+#include <vulkan/vk_format_utils.h>
+
 namespace vk_utils {
 
 inline bool format_is_multiplane(vk::Format format)
 {
-  // FIXME:
-  return false;
+  return FormatIsMultiplane(static_cast<VkFormat>(format));
 }
 
 inline bool format_is_compatible(vk::Format format1, vk::Format format2)
 {
-  // FIXME:
-  return format1 == format2;
+  return FormatCompatibilityClass(static_cast<VkFormat>(format1)) == FormatCompatibilityClass(static_cast<VkFormat>(format2));
 }
 
 } // namespace vk_utils
+
+#endif // CW_DEBUG
