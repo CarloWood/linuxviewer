1899b7264cf216e73534a7239b3baafaefa0cf75
Carlo Wood <carlo@alinoe.com>
2022-04-20 17:34:06 +0200

    Turn ImmediateSubmitData into a class.

--- src/vulkan/immediatesubmit/ImmediateSubmitData.h
+++ src/vulkan/immediatesubmit/ImmediateSubmitData.h
@@ -7,13 +7,19 @@
 namespace vulkan {
 
 // This struct contains the data needed to start an ImmediateSubmit task.
-struct ImmediateSubmitData
+class ImmediateSubmitData
 {
+ public:
   static constexpr vk::CommandPoolCreateFlags::MaskType pool_type = VK_COMMAND_POOL_CREATE_TRANSIENT_BIT;
   using record_function_type = std::function<void(vulkan::CommandBufferWriteAccessType<pool_type>)>;
 
-  vulkan::QueueRequestKey queue_request_key;    // Key that uniquely maps to a queue (request/reply) to use.
-  record_function_type record_function;         // Callback function that will record the command buffer.
+ private:
+  vulkan::QueueRequestKey m_queue_request_key;  // Key that uniquely maps to a queue (request/reply) to use.
+  record_function_type m_record_function;       // Callback function that will record the command buffer.
+
+ public:
+  ImmediateSubmitData(vulkan::QueueRequestKey queue_request_key, std::function<void(vulkan::CommandBufferWriteAccessType<pool_type>)>&& record_function) :
+    m_queue_request_key(queue_request_key), m_record_function(std::move(record_function)) { }
 };
 
 } // namespace vulkan
