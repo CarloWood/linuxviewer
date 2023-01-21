5425daaf45f12f0a959160c5fde9db3a943423f5
Carlo Wood <carlo@alinoe.com>
2023-01-21 23:26:53 +0100

    WIP

--- src/vulkan/pipeline/AddFragmentShader.cxx
+++ src/vulkan/pipeline/AddFragmentShader.cxx
@@ -6,7 +6,9 @@ namespace vulkan::pipeline {
 
 void AddFragmentShader::register_AddFragmentShader_with(task::PipelineFactory* pipeline_factory) const
 {
-  //FIXME implement
+  // Register vectors here of they're ever added.
+  // FIXME: does AddFragmentShader every have such vectors? And if not, shouldn't AddFragmentShader be removed?
+  //pipeline_factory->add_to_flat_create_info(...);
 }
 
 #ifdef CWDEBUG
