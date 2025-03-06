1c2751270f2ed08d19b57d0122f75b4517754682
Carlo Wood <carlo@alinoe.com>
2025-03-06 16:53:44 +0100

    Add and use PRAGMA_DIAGNOSTIC_PUSH_IGNORE_nullability_completeness

--- src/vulkan/VmaUsage.cxx
+++ src/vulkan/VmaUsage.cxx
@@ -2,6 +2,6 @@
 #include "utils/macros.h"
 
 #define VMA_IMPLEMENTATION
-PRAGMA_DIAGNOSTIC_PUSH_IGNORE("-Wnullability-completeness")
+PRAGMA_DIAGNOSTIC_PUSH_IGNORE_nullability_completeness
 #include "vk_mem_alloc.h"
 PRAGMA_DIAGNOSTIC_POP
